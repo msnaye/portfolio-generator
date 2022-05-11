4474abc08984d031c4954eec97d500f84b68093e
msnaye <99111936+msnaye@users.noreply.github.com>
2022-05-11 02:47:40 -0500

    added inquirer prompt

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+//const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+//const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//fs.writeFile('./index.html', err => {
+    //if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+  //});
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+  
