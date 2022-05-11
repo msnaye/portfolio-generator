399321d138f017c8a207fb73e2120a01633dd23a
msnaye <99111936+msnaye@users.noreply.github.com>
2022-05-11 01:49:19 -0500

    fixed code

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
